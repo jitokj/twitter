4193470978cb5f48fd445cdba9303f51c5efbd74
jito <jitokj3@gmail.com>
2020-11-25 11:37:27 +0530

    edited manifest

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "./Widget.css";
+import SearchIcon from '@material-ui/icons/Search';
+import {TwitterTimelineEmbed,TwitterShareButton,TwitterTweetEmbed} from "react-twitter-embed";
 
 const Widget = () => {
     return (
         <div className="widget">
 
-        <h1>Widget</h1>
+       <div className="widget-input">
+           <SearchIcon className="widget__searchIcon" />
+           <input type="text" placeholder="Search Twitter" />
+       </div>
+       <div className="widget__widgetContainer">
+           <h2>What's happening ?</h2>
+           <TwitterTweetEmbed tweetId={"858551177860055040"} />
+       </div>
             
         </div>
     );
