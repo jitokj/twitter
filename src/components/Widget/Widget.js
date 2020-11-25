cc7ac10ad480d38433f52f878ee890c601bc9ac2
jito <jitokj3@gmail.com>
2020-11-25 16:16:22 +0530

    added widget

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -7,13 +7,15 @@ const Widget = () => {
     return (
         <div className="widget">
 
-       <div className="widget-input">
+       <div className="widget__input">
            <SearchIcon className="widget__searchIcon" />
            <input type="text" placeholder="Search Twitter" />
        </div>
        <div className="widget__widgetContainer">
            <h2>What's happening ?</h2>
-           <TwitterTweetEmbed tweetId={"858551177860055040"} />
+           <TwitterTweetEmbed tweetId={"1331485043919732736"} />
+           <TwitterTimelineEmbed  sourceType="profile" screenName="jitokj" noScrollbar placeholder="Loading....😎" options={{height: 400}} />
+           <TwitterShareButton  url={"https://twitter.com/reactjs"} options={{text:"Reactjs is awesome",via:"jitokj"}} />
        </div>
             
         </div>
