c536a55bcb148690a9f92368786260ffda42332e
jito <jitokj3@gmail.com>
2020-11-26 09:41:11 +0530

    realtime post with firestore

--- src/components/Feed/TweetBox/TweetBox.js
+++ src/components/Feed/TweetBox/TweetBox.js
@@ -2,10 +2,25 @@ import { Button,Avatar } from '@material-ui/core';
 import Logo from "../../../images/man.png";
 import React, { useState } from 'react';
 import "./TweetBox.css";
+import db from "../../../firebase";
 
 const TweetBox = () => {
     const [tweetMessage,setTweetMessage] = useState("");
-    const [tweetImage,setTweetMessage] = useState("");
+    const [tweetImage,setTweetImage] = useState("");
+    const sendTweet = (e)=>{
+        e.preventDefault();
+        db.collection('posts').add({
+            displayName: 'JITO K J',
+            userName: 'jitokj',
+            verified: true,
+            text: tweetMessage,
+            image: tweetImage,
+            avatar: "https://img.icons8.com/ultraviolet/40/000000/select-user.png"
+
+        })
+        setTweetImage("");
+        setTweetMessage("");
+    }
     return (
         <div className="tweetbox">
             <form>
@@ -14,8 +29,8 @@ const TweetBox = () => {
                     <input onChange={(e)=>setTweetMessage(e.target.value)} value={tweetMessage} placeholder="What's happening ? " type="text" />
                    
                 </div>
-                <input className="tweetbox__imageInput" placeholder="Optional : Enter Image Url" type="text" />
-                <Button className="tweetbox__tweetButton">Tweet</Button>
+                <input value={tweetImage} onChange={(e)=>setTweetImage(e.target.value)} className="tweetbox__imageInput" placeholder="Optional : Enter Image Url" type="text" />
+                <Button type="submit" onClick={sendTweet} className="tweetbox__tweetButton">Tweet</Button>
             </form>
         </div>
     );
